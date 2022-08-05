d273445d2df2c3dc6c0c117dda1b121d6fa9766f
Roman <roman.kocenko.2004@mai.ru>
2022-08-05 17:29:57 +0300

    Infinite loop with 1 minute waiting done

--- Microservices/main.go
+++ Microservices/main.go
@@ -10,40 +10,27 @@ import (
 
 func main() {
 	var (
-		body             models.Content
-		dataToInsertInDB []models.Content
-		isDone           bool
-		wg               sync.WaitGroup
+		body models.Content
+		wg   sync.WaitGroup
 	)
 
-	ticker := time.NewTicker(100 * time.Millisecond)
-	stopCh := make(chan bool)
+	ticker := time.NewTicker(time.Minute)
 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 
 		db := database.New()
-		for !isDone {
+		for {
 			select {
-			case <-stopCh:
-				isDone = true
 			case <-ticker.C:
 				bodyJSON := controller.GetBodyRequest()
 				body = controller.UnMarshal(bodyJSON)
-				dataToInsertInDB = append(dataToInsertInDB, body)
-			}
-		}
-
-		for i := 0; i < len(dataToInsertInDB); i++ {
-			for j := 0; j < len(dataToInsertInDB[i].Content); j++ {
-				db.Add(dataToInsertInDB[i].Content[j])
+				for j := 0; j < len(body.Content); j++ {
+					db.Add(body.Content[j])
+				}
 			}
 		}
 	}()
-
-	time.Sleep(601 * time.Millisecond)
-	stopCh <- true
-	ticker.Stop()
 	defer wg.Wait()
 }
